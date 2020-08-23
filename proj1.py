a5f9e670505d018b0d24a8e2a8e99fd19d65de42
ashish-cora <gwara.boy@gmail.com>
2020-08-23 12:45:33 +0545

    pong

---
+++ proj1.py
@@ -0,0 +1,136 @@
+import turtle
+x = 0
+y = 0
+
+wn = turtle.Screen()
+wn.title("pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+paddle_a.shapesize(stretch_len=1, stretch_wid=5)
+
+# paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.penup()
+paddle_b.goto(350, 0)
+paddle_b.shapesize(stretch_len=1, stretch_wid=5)
+
+# ball
+ball = turtle.Turtle()
+ball.speed(5)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = .2
+ball.dy = -.2
+
+def showscore():
+    # pen
+    pen = turtle.Turtle()
+    pen.speed(0)
+    pen.color("white")
+    pen.penup()
+    pen.hideturtle()
+    pen.goto(0, 250)
+    pen.clear()
+    pen.write("player A:%s" % x + "      player B:%s" % y)
+
+
+# function
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+
+# keyboard biniding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
+
+showscore()
+while True:
+
+    wn.update()
+
+    # move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+    #ball bouncing from paddle
+    if ball.xcor()>340 and (ball.ycor()<paddle_b.ycor()+50 and ball.ycor()>paddle_b.ycor()-50):
+        ball.setx(340)
+        ball.dx *= -1
+        ball.dy *= 1
+
+    if ball.xcor() < -340 and (ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50):
+        ball.setx(-340)
+        ball.dx *= -1
+        ball.dy *= 1
+        #paddle checking
+    if paddle_a.ycor()>250:
+        paddle_a.sety(250)
+    if paddle_b.ycor()>250:
+        paddle_b.sety(250)
+    if paddle_a.ycor()<-250:
+        paddle_a.sety(-250)
+    if paddle_b.ycor()<-250:
+        paddle_b.sety(-250)
+
+    #ball checking
+    if ball.xcor()>390:
+        ball.setx(0)
+        ball.dx *= -1
+        x+=1
+        showscore()
+
+
+
+
+    if ball.xcor()<-390:
+        ball.setx(0)
+        ball.dx*=-1
+        y+=1
+        showscore()
+
+
